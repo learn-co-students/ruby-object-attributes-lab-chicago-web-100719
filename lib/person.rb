88bb6822ecd94d43fffb844050a168a1524ed729
ladeeluc <laura.hatch.lh@gmail.com>
2019-10-08 19:00:23 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+
+class Person
+
+    def name= (teach)
+        @name = teach
+
+end
+        def name
+            @name
+        end
+
+    def job= (worker)
+        @job = worker
+    end
+
+        def job
+            @job
+        end
+end
